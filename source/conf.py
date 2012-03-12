ebdc24714b6f99153dda6bceb482006027487e3d
veit <kontakt@veit-schiele.de>
2012-03-12 11:12:07 +0000

    Updated copyright

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Plone-Nutzerhandbuch'
-copyright = u'2010–2011, Veit Schiele'
+copyright = u'2010–2012, Veit Schiele'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
