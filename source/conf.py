0d6370245e8702a207d8f82430d899345be8b0e6
veit <kontakt@veit-schiele.de>
2014-05-12 04:49:25 +0200

    Added graphviz extension

--- source/conf.py
+++ source/conf.py
@@ -26,7 +26,11 @@ import sphinx_rtd_theme
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo']
+extensions = [
+    'sphinx.ext.todo',
+    'sphinx.ext.graphviz',
+    ]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
